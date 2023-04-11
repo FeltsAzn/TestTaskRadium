f380d82c50d8fd206098fb26e1b77d5c3fdae6d7
Tim <timkin99@mail.ru>
2023-04-11 21:20:27 +0300

    refactor class, separation methods

--- src/hash_sum.py
+++ src/hash_sum.py
@@ -1,73 +1,42 @@
-"""Calculate hash sum for downloaded files.
-
-Classes:
-- HashSum: class which accumulate methods to find files hash sums
-"""
-
-
 import json
+import os
 from hashlib import sha256
-from pathlib import Path
 
 
 class HashSum:
-    """Calculate hash sum.
+    def __init__(self, path):
+        self.path = path
 
-    Methods:
-    - start_analyze: start analyze repository to find files hash sums
-    - find_files: recursive searching repository
-    - find_hash_sum: calculate file hash sum
-    - save_calculate_hash: save found hash sums to json file
-    """
+    def start_analyze(self) -> None:
+        paths = self.find_files()
+        hash_sums = self.calculate_files_hash(paths)
+        self.save_calculated_hash(hash_sums)
 
-    def __init__(self):
-        self.hash_sum_table = {}
+    def find_files(self) -> list[tuple[str, str]]:
+        files_paths = []
+        for folder_name, _, filenames in os.walk(self.path):
+            for filename in filenames:
+                file_path = os.path.join(folder_name, filename)
+                files_paths.append((filename, file_path))
+        return files_paths
 
-    def start_analyze(self, path: str) -> None:
-        """Start main process.
+    def calculate_files_hash(self, files: list[tuple]) -> dict:
+        hash_sum_table = {}
+        for filename, file_path in files:
+            hash_sum = self.hash_sum(file_path=file_path)
+            hash_sum_table[filename] = {
+                'hash_sum': hash_sum,
+                'path': file_path,
+            }
+        return hash_sum_table
 
-        Args:
-            path (str): target path, which contain downloaded files
-        """
-        self.find_files(path=path)
-        self.save_calculate_hash()
-
-    def find_files(self, path: str) -> None:
-        """Recursive repository searching.
-
-        Args:
-            path (str): path to searching
-        """
-        context = Path(path)
-        if context.iterdir():
-            for found_file in context.iterdir():
-                file_path = found_file.as_posix()
-                filename = found_file.name
-                if found_file.is_dir():
-                    # recursion entry
-                    self.find_files(path=file_path)
-                else:
-                    # recursion exit
-                    hash_sum = self.find_hash_sum(file_path=file_path)
-                    self.hash_sum_table[filename] = {
-                        'hash_sum': hash_sum,
-                        'path': file_path,
-                    }
-
-    def find_hash_sum(self, file_path: str) -> str:
-        """Calculate hash sum.
-
-        Args:
-            file_path (str): file for calculating hash sum
-
-        Returns:
-            a hash string
-        """
+    @staticmethod
+    def hash_sum(file_path: str) -> str:
         with open(file_path, 'rb') as downloaded_file:
             file_bytes = downloaded_file.read()
         return sha256(file_bytes).hexdigest()
 
-    def save_calculate_hash(self) -> None:
-        """Save calculated result."""
+    @staticmethod
+    def save_calculated_hash(hash_sums: dict) -> None:
         with open('repository/hash_sums.json', 'w') as result_file:
-            json.dump(self.hash_sum_table, result_file, indent=4)
+            json.dump(hash_sums, result_file, indent=4)
